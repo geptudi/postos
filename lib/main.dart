aa24f31820109e8603edf9c04c4e0b123a7f4e64
josuemoraisgh <josuemorais@gmail.com>
2023-11-17 07:55:15 -0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animações',
+      title: 'GEPT - Postos de Assistência',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
--- lib/pages/expansion_page.dart
+++ lib/pages/expansion_page.dart
@@ -14,7 +14,28 @@ class _ExpansionPageState extends State<ExpansionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Produtos')),
+      appBar: AppBar(
+        title: Row(children: <Widget>[
+          const Text('Posto: '),
+          DropdownButton<String>(
+            dropdownColor: Theme.of(context).colorScheme.background,
+            style: const TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                decorationColor: Colors.black),
+            items: const <DropdownMenuItem<String>>[
+              DropdownMenuItem<String>(
+                value: "Eurípedes Barsanulfo (Morada Nova)",
+                child: Text("Eurípedes Barsanulfo (Morada Nova)"),
+              ),
+            ],
+            onChanged: (String? novoItemSelecionado) {
+              if (novoItemSelecionado != null) {}
+            },
+            value: "Eurípedes Barsanulfo (Morada Nova)",
+          ),
+        ]),
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(12),
@@ -40,7 +61,8 @@ class _ExpansionPageState extends State<ExpansionPage> {
                       padding: const EdgeInsets.only(bottom: 12.0),
                       child: Text(product.description),
                     ),
-                    Image.network('https://picsum.photos/id/${product.id}/500/300'),
+                    Image.network(
+                        'https://picsum.photos/id/${product.id}/500/300'),
                   ],
                 ),
               );
