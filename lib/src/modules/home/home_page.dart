eab7c65204c3c6cfd5a02e361251322dff1d5c8a
josuemoraisgh <josuemorais@gmail.com>
2023-11-18 16:39:44 -0300

    Versão 0.3.4

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:icon_badge/icon_badge.dart';
@@ -17,6 +18,7 @@ class _HomePageState extends State<HomePage> {
   final controller = Modular.get<HomeController>();
   late Future<bool> initedState;
   List<Assistido> assistidoList = [];
+  List<int> aux = [];
 
   @override
   void initState() {
@@ -75,47 +77,57 @@ class _HomePageState extends State<HomePage> {
                         body: SingleChildScrollView(
                           child: Padding(
                             padding: const EdgeInsets.all(12),
-                            child: ValueListenableBuilder(
-                              valueListenable: controller.isExpanded,
-                              builder: (BuildContext context, bool activeTag,
-                                      Widget? child) =>
-                                  ExpansionPanelList.radio(
-                                expansionCallback:
-                                    (int index, bool isExpanded) {
-                                  controller.isExpanded.value =
-                                      !controller.isExpanded.value;
-                                },
-                                children: assistidoList
-                                    .map<ExpansionPanel>((Assistido product) {
-                                  return ExpansionPanelRadio(
-                                    backgroundColor: Colors.white,
-                                    // isExpanded: product.isExpanded,
-                                    value: product.ident,
-                                    canTapOnHeader: true,
-                                    headerBuilder: (BuildContext context,
-                                        bool isExpanded) {
-                                      return ListTile(
-                                        leading: CircleAvatar(
-                                            child:
-                                                Text(product.ident.toString())),
-                                        title: Text(
-                                            'Família com ${product.nomesMoradores.split(';').length} moradores sendo:\n0 - Adultos\n0 - Adolescente(s) e\n0 - Crianças\nResponsável: ${product.nomeM1}'),
-                                      );
-                                    },
-                                    body: Column(
-                                      mainAxisSize: MainAxisSize.max,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                            '${product.logradouro}: ${product.endereco} nº ${product.numero}\nBairro: ${product.bairro}\nTelefone: ${product.fone}'),
-                                      ],
-                                    ),
-                                  );
-                                }).toList(),
-                              ),
+                            child: ExpansionPanelList.radio(
+                              expansionCallback: (int index, bool isExpanded) {
+                                controller.isExpanded.value =
+                                    !controller.isExpanded.value;
+                              },
+                              children: assistidoList
+                                  .map<ExpansionPanel>((Assistido product) {
+                                return ExpansionPanelRadio(
+                                  backgroundColor: Colors.white,
+                                  // isExpanded: product.isExpanded,
+                                  value: product.ident,
+                                  canTapOnHeader: true,
+                                  headerBuilder:
+                                      (BuildContext context, bool isExpanded) {
+                                    var st = product.datasNasc.substring(
+                                        0,
+                                        product.datasNasc.isEmpty
+                                            ? 0
+                                            : product.datasNasc.length - 1);
+                                    aux = ([product.dataNascM1] + st.split(';'))
+                                        .map(
+                                      (e) {
+                                        return e == ""
+                                            ? calculateAge(
+                                                DateFormat('dd/MM/yyyy')
+                                                    .parse("1/1/1900"))
+                                            : calculateAge(
+                                                DateFormat('dd/MM/yyyy')
+                                                    .parse(e));
+                                      },
+                                    ).toList();
+                                    return ListTile(
+                                      leading: CircleAvatar(
+                                          child:
+                                              Text(product.ident.toString())),
+                                      title: Text(
+                                          'Família com ${product.nomesMoradores.split(';').length} moradores sendo:\n0 - Adultos\n0 - Adolescente(s) e\n0 - Crianças\nResponsável: ${product.nomeM1}'),
+                                    );
+                                  },
+                                  body: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          '${product.logradouro}: ${product.endereco} nº ${product.numero}\nBairro: ${product.bairro}\nTelefone: ${product.fone}'),
+                                    ],
+                                  ),
+                                );
+                              }).toList(),
                             ),
                           ),
                         ),
@@ -127,5 +139,22 @@ class _HomePageState extends State<HomePage> {
                   ),
       ),
     );
+  }
+
+  int calculateAge(DateTime birthDate) {
+    DateTime currentDate = DateTime.now();
+    int age = currentDate.year - birthDate.year;
+    int month1 = currentDate.month;
+    int month2 = birthDate.month;
+    if (month2 > month1) {
+      age--;
+    } else if (month1 == month2) {
+      int day1 = currentDate.day;
+      int day2 = birthDate.day;
+      if (day2 > day1) {
+        age--;
+      }
+    }
+    return age;
   }
 }
