1d9379648fce77e68c00a4648d89733f5a64c724
josuemoraisgh <josuemorais@gmail.com>
2023-11-23 20:17:12 -0300

    Versão 2.1.0

--- lib/src/modules/home/modelsview/template_page.dart
+++ lib/src/modules/home/modelsview/template_page.dart
@@ -55,128 +55,120 @@ class _TemplatePageState extends State<TemplatePage> {
       builder: (BuildContext context, BoxConstraints constraints) {
         return Container(
           color: groundColor,
-          child: SingleChildScrollView(
-            controller: scrollController,
-            child: Stack(
-              alignment: AlignmentDirectional.topCenter,
-              children: [
-                SizedBox(
-                  width: constraints.maxWidth > tamDesejado
-                      ? tamDesejado
-                      : constraints.maxWidth,
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 50, top: 0, right: 50, bottom: 0),
-                        child: Card(
-                          color: headerColor, //Colors.green,
-                          elevation: 8,
-                          margin: const EdgeInsets.all(0.0),
-                          shape: const OutlineInputBorder(
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20)),
-                            borderSide: borderSideValue,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              if (widget.isLeading != null)
-                                IconButton(
-                                  icon: const Icon(Icons.arrow_back_outlined),
-                                  color: Colors.white,
-                                  onPressed: () {
-                                    Modular.to.pop();
-                                  },
-                                ),
-                              if (widget.header != null)
-                                Flexible(
-                                  child: widget.header!,
-                                ),
-                            ],
-                          ),
-                        ),
+          child: Center(
+            child: SizedBox(
+              width: constraints.maxWidth > tamDesejado
+                  ? tamDesejado
+                  : constraints.maxWidth,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 50, top: 0, right: 50, bottom: 0),
+                    child: Card(
+                      color: headerColor, //Colors.green,
+                      elevation: 8,
+                      margin: const EdgeInsets.all(0.0),
+                      shape: const OutlineInputBorder(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20)),
+                        borderSide: borderSideValue,
                       ),
-                      Flexible(
-                        child: Card(
-                          color: Colors.white,
-                          elevation: 8,
-                          shape: const OutlineInputBorder(
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(20),
-                                  topRight: Radius.circular(20),
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              borderSide: BorderSide(color: Colors.white)),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                left: 20, top: 10, right: 20, bottom: 20),
-                            child: Form(
-                              key: _formKey,
-                              onChanged: () {
-                                if (_formKey.currentState!.validate()) {
-                                  List<ValueNotifier<String>> answer =
-                                      formFieldkey.currentState!.value!;
-                                  answerNotifier.value = answer;
-                                } else {
-                                  answerNotifier.value = [];
-                                }
-                              },
-                              autovalidateMode:
-                                  AutovalidateMode.onUserInteraction,
-                              child: FormField<List<ValueNotifier<String>>>(
-                                key: formFieldkey,
-                                initialValue: controller.answerAux.value,
-                                validator:
-                                    (List<ValueNotifier<String>>? value) {
-                                  if (value == null) {
-                                    return 'Por favor responda todas as questões';
-                                  } else {
-                                    final count = value
-                                        .where((item) => item.value != "")
-                                        .length;
-                                    if (count != value.length) {
-                                      return 'Por favor responda todas as questões';
-                                    }
-                                  }
-                                  return (null);
-                                },
-                                builder:
-                                    (FormFieldState<List<ValueNotifier<String>>>
-                                        state) {
-                                  List<Widget> itens =
-                                      widget.itens(controller, formFieldkey);
-                                  return ListView.builder(
-                                    shrinkWrap: true,
-                                    itemCount: itens.length,
-                                    itemBuilder:
-                                        (BuildContext context, int index) =>
-                                            itens[index],
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Row(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Spacer(flex: 1),
-                          if (widget.hasProx != null)
-                            Container(
-                              padding: const EdgeInsets.only(
-                                  left: 0, top: 0, right: 10, bottom: 0),
-                              alignment: Alignment.bottomRight,
-                              child: _proximaButton(widget.hasProx!),
+                          if (widget.isLeading != null)
+                            IconButton(
+                              icon: const Icon(Icons.arrow_back_outlined),
+                              color: Colors.white,
+                              onPressed: () {
+                                Modular.to.pop();
+                              },
+                            ),
+                          if (widget.header != null)
+                            Flexible(
+                              child: widget.header!,
                             ),
                         ],
                       ),
+                    ),
+                  ),
+                  Flexible(
+                    child: Card(
+                      color: Colors.white,
+                      elevation: 8,
+                      shape: const OutlineInputBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20)),
+                          borderSide: BorderSide(color: Colors.white)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            left: 20, top: 10, right: 20, bottom: 20),
+                        child: Form(
+                          key: _formKey,
+                          onChanged: () {
+                            if (_formKey.currentState!.validate()) {
+                              List<ValueNotifier<String>> answer =
+                                  formFieldkey.currentState!.value!;
+                              answerNotifier.value = answer;
+                            } else {
+                              answerNotifier.value = [];
+                            }
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          child: FormField<List<ValueNotifier<String>>>(
+                            key: formFieldkey,
+                            initialValue: controller.answerAux.value,
+                            validator: (List<ValueNotifier<String>>? value) {
+                              if (value == null) {
+                                return 'Por favor responda todas as questões';
+                              } else {
+                                final count = value
+                                    .where((item) => item.value != "")
+                                    .length;
+                                if (count != value.length) {
+                                  return 'Por favor responda todas as questões';
+                                }
+                              }
+                              return (null);
+                            },
+                            builder:
+                                (FormFieldState<List<ValueNotifier<String>>>
+                                    state) {
+                              List<Widget> itens =
+                                  widget.itens(controller, formFieldkey);
+                              return ListView.builder(
+                                //shrinkWrap: true,
+                                itemCount: itens.length,
+                                itemBuilder:
+                                    (BuildContext context, int index) =>
+                                        itens[index],
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      const Spacer(flex: 1),
+                      if (widget.hasProx != null)
+                        Container(
+                          padding: const EdgeInsets.only(
+                              left: 0, top: 0, right: 10, bottom: 0),
+                          alignment: Alignment.bottomRight,
+                          child: _proximaButton(widget.hasProx!),
+                        ),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         );
