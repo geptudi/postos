6926fdd561f5bd2c82719389a65219a6803aa4aa
josuemoraisgh <josuemorais@gmail.com>
2025-11-30 16:47:38 -0300

    refactor postos.json

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:postos/src/app_module.dart';
 import 'package:postos/src/app_widget.dart';
+import 'package:postos/src/models/parameters.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
